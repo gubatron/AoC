738565765d9d8700045d45fb0f70a6f6769787d0
gubatron <gubatron@gmail.com>
2020-12-03 09:46:25 -0700

    Procedural accumulator -> Functional accumulator refactor on Day02

--- 2020/src/main/java/com/gubatron/aoc/_2020/Day02.java
+++ 2020/src/main/java/com/gubatron/aoc/_2020/Day02.java
@@ -1,10 +1,13 @@
 package com.gubatron.aoc._2020;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static com.gubatron.aoc._2020.Utils.readStringList;
 
 public class Day02 {
     static final Pattern policy_pass_pattern = Pattern.compile("([0-9]+)-([0-9]+) ([a-z]+): ([a-z]+)");
@@ -39,48 +42,26 @@ public class Day02 {
     public static boolean isPasswordValid2(PolicyPass pp) {
         int index_1 = pp.min - 1;
         int index_2 = pp.max - 1;
-        return (pp.pass.charAt(index_1) == pp.c &&
-                pp.pass.charAt(index_2) != pp.c) ||
-                (pp.pass.charAt(index_1) != pp.c &&
-                        pp.pass.charAt(index_2) == pp.c);
+        return pp.pass.charAt(index_1) == pp.c ^ pp.pass.charAt(index_2) == pp.c;
     }
 
     public static List<PolicyPass> readInput(File f) throws IOException {
-        List<PolicyPass> result = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        while (br.ready()) {
-            String line = br.readLine();
-            PolicyPass pp = build(line);
-            result.add(pp);
-        }
-        return result;
+        List<String> strings = readStringList(f);
+        return strings.stream().map(Day02::build).collect(Collectors.toList());
     }
 
     public static int part1(List<PolicyPass> list) {
-        int validCount = 0;
-        for (PolicyPass pp : list) {
-            if (isPasswordValid(pp)) {
-                validCount++;
-            }
-        }
-        return validCount;
+        return list.stream().map(pp -> (isPasswordValid(pp)) ? 1 : 0).reduce(Integer::sum).orElse(0);
     }
 
     public static int part2(List<PolicyPass> list) {
-        int validCount = 0;
-        for (PolicyPass pp : list) {
-            if (isPasswordValid2(pp)) {
-                validCount++;
-            }
-        }
-        return validCount;
+        return list.stream().map(pp -> (isPasswordValid2(pp)) ? 1 : 0).reduce(Integer::sum).orElse(0);
     }
-
 
     public static void main(String[] args) throws IOException {
         List<PolicyPass> policyPasses = readInput(new File("resources/input_day_02.txt"));
         //List<PolicyPass> policyPasses = readInput(new File("resources/sample_day_02.txt"));
-        System.out.println("Part 1: " + part1(policyPasses));
-        System.out.println("Part 2: " + part2(policyPasses));
+        System.out.println("Part 1: " + part1(policyPasses)); // Part 1: 628
+        System.out.println("Part 2: " + part2(policyPasses)); // Part 2: 705
     }
 }
