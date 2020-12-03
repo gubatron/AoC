3021432d8b6533719ea4f13beba7080572f571fa
gubatron <gubatron@gmail.com>
2020-12-02 23:48:05 -0700

    2020/3 - Toboggan Trajectory
    DAY 03
    Part 1 Trees Found: 159
    
    ==============================
    
    Trees @ 0 => 86
    Trees @ 1 => 159
    Trees @ 2 => 97
    Trees @ 3 => 88
    Trees @ 4 => 55
    Part 2:
    6419669520

--- 2020/src/com/gubatron/aoc/_2020/Day03.java
+++ 2020/src/com/gubatron/aoc/_2020/Day03.java
@@ -5,30 +5,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Day03 {
-    static class Something {
-
-    }
-    public static List<Something> readInput(File f) throws IOException {
-        List<Something> result = new ArrayList<>();
+    public static List<String> readInput(File f) throws IOException {
+        List<String> result = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(f));
+
         while (br.ready()) {
-            String line = br.readLine();
-            //
-            //result.add(pp);
+            String line = br.readLine().strip();
+            result.add(line);
         }
         return result;
     }
 
-    public void part1(List<Something> data) {
+    public static int part1(String[] forest, int right_moves, int down_moves) {
+        int pos_x = 0;
+        int pos_y = 0;
+        int line_width = forest[0].length();
 
+        int trees = 0;
+        while (pos_y < forest.length - 1) {
+            pos_x += right_moves;
+            pos_x = (pos_x % line_width);
+
+            pos_y += down_moves;
+
+            String line = forest[pos_y];
+
+            if (line.charAt(pos_x) == '#') {
+                trees++;
+            }
+        }
+        return trees;
     }
 
-    public void part2(List<Something> data) {
+    public static long part2(String[] forest) {
+        int[][] slopes = {
+                {1, 1},
+                {3, 1},
+                {5, 1},
+                {7, 1},
+                {1, 2}
+        };
+        int[] trees_found = new int[slopes.length];
 
+        long result = 1;
+        for (int seq_i = 0; seq_i < slopes.length; seq_i++) {
+            trees_found[seq_i] = part1(forest, slopes[seq_i][0], slopes[seq_i][1]);
+            System.out.println("Trees @ " + seq_i + " => " + trees_found[seq_i]);
+            result = result * trees_found[seq_i];
+        }
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
-        List<Something> data = readInput(new File("sample_day_03.txt"));
-        //List<Something> data = readInput(new File("input_day_03.txt"));
+        //List<String> lines = readInput(new File("inputs/sample_day_03.txt"));
+        List<String> lines = readInput(new File("inputs/input_day_03.txt"));
+        int width = lines.get(0).length();
+        int height = lines.size();
+        String[] forest = lines.toArray(new String[0]);
+        System.out.println("DAY 03");
+        System.out.println("Part 1 Trees Found: " + part1(forest, 3, 1));
+        System.out.println("\n==============================\n");
+        System.out.println("Part 2:\n" + part2(forest));
     }
 }
