37a9124ae56babd01814dc42927d35d370996b5e
gubatron <gubatron@gmail.com>
2020-12-07 21:58:20 -0700

    Fixes/Optimizations on boilerplate template

--- 2020/src/main/java/com/gubatron/aoc/_2020/DayN.java
+++ 2020/src/main/java/com/gubatron/aoc/_2020/DayN.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static com.gubatron.aoc._2020.Utils.readStringList;
+import static com.gubatron.aoc._2020.Utils.readStringsBySeparator;
 
 public class DayN {
     public static long part1() {
@@ -16,8 +16,8 @@ public class DayN {
     }
 
     public static void main(String[] args) throws IOException {
-        List<String> lines = readStringList(new File("resources/sample_day_N.txt"));
-        //List<String> lines = readStringList(new File("resources/input_day_N.txt"));
+        List<String> lines = readStringsBySeparator(new File("resources/sample_day_MM.txt"),"\n");
+        //List<String> lines = readStringsBySeparator(new File("resources/input_day_NN.txt"),"\n");
         System.out.println("DAY NN");
         System.out.println("Part 1: " + part1());
         System.out.println("==============================");
