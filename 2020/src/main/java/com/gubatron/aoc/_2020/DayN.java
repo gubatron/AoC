586460337da3f4f4632e7463e1b4743f6969d9a7
gubatron <gubatron@gmail.com>
2020-12-18 03:07:31 -0700

    template fix

--- 2020/src/main/java/com/gubatron/aoc/_2020/DayN.java
+++ 2020/src/main/java/com/gubatron/aoc/_2020/DayN.java
@@ -7,20 +7,20 @@ import java.util.List;
 import static com.gubatron.aoc._2020.Utils.readStringsBySeparator;
 
 public class DayN {
-    public static void main(String[] args) throws IOException {
-        List<String> lines = readStringsBySeparator(new File("resources/sample_day_MM.txt"),"\n");
-        //List<String> lines = readStringsBySeparator(new File("resources/input_day_NN.txt"),"\n");
-        System.out.println("DAY NN");
-        System.out.println("Part 1: " + part1());
-        System.out.println("==============================");
-        System.out.println("Part 2: " + part2());
-    }
-
     public static long part1() {
         return 0;
     }
 
     public static long part2() {
         return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = readStringsBySeparator(new File("resources/sample_day_NN.txt"),"\n");
+        //List<String> lines = readStringsBySeparator(new File("resources/input_day_NN.txt"),"\n");
+        System.out.println("DAY N - ");
+        System.out.println("Part 1: " + part1());
+        System.out.println("==============================");
+        System.out.println("Part 2: " + part2());
     }
 }
