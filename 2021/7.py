bca4a35a6d278ba83d5fbada138ffcd748b3c32d
gubatron <gubatron@gmail.com>
2021-12-07 08:36:14 -0700

    3 less lines

--- 2021/7.py
+++ 2021/7.py
@@ -5,12 +5,9 @@ crabPositions = list(map(int, aoc.readFileToStringList("7.txt")[0].split(',')))
 crabPositions.sort()
 median = int(statistics.median(crabPositions))
 mean = floor(statistics.mean(crabPositions))
-totalFuel1 = 0
-totalFuel2 = 0
+ANS1 = ANS2 = 0
 for crabPos in crabPositions:
-    totalFuel1 += abs(crabPos - median)
-    totalFuel2 += sum(range(1, 1 + abs(crabPos - mean)))
-ANS1 = totalFuel1
-ANS2 = totalFuel2
+    ANS1 += abs(crabPos - median)
+    ANS2 += sum(range(1, 1 + abs(crabPos - mean)))
 print("ans1={}".format(ANS1))  # ans1=364898
 print("ans2={}".format(ANS2))  # ans2=104149091
