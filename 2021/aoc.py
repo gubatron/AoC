0f51342e3434326c77393ef5afa5dda754f6b841
Angel Leon <gubatron@gmail.com>
2021-12-11 10:54:00 -0700

    Added utility functions that come up a lot on aoc.py
     - Read an int matrix from the file
     - get surrounding coordinates in a matrix cell, takes a flag to consider diagonals

--- 2021/aoc.py
+++ 2021/aoc.py
@@ -1,10 +1,11 @@
 def readFileToStringList(path, stripped=True):
-  fp = open(path, mode='r', buffering=4096)
-  result = fp.readlines()
-  if stripped:
-      result = [s.strip() for s in result]
-  fp.close()
-  return result
+    fp = open(path, mode='r', buffering=4096)
+    result = fp.readlines()
+    if stripped:
+        result = [s.strip() for s in result]
+    fp.close()
+    return result
+
 
 def readStringsBySeparator(path, separator, stripped=True):
     stringList = readFileToStringList(path, stripped)
@@ -13,10 +14,40 @@ def readStringsBySeparator(path, separator, stripped=True):
         return bigString.split(separator)
     return [s.strip() for s in bigString.split(separator)]
 
-def readIntList(path, stripped=False):
+
+def readIntList(path, stripped=True):
     stringList = readFileToStringList(path, stripped)
     return list(map(int, stringList))
 
+
+def readIntMatrix(path, stripped=True):
+    data = readFileToStringList(path)
+    G = []
+    for l in list(map(list, data)):
+        G.append(list(map(int, l)))
+    return G
+
+
+def getSurroundingCoords(r, c, graph, includeDiagonals=False):
+    surrounding = []
+    deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    if includeDiagonals:
+        deltas += [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    rows, cols = len(graph), len(graph[0])
+    for (dx, dy) in deltas:
+        if r + dx >= 0 and r + dx < rows and c + dy >= 0 and c + dy < cols:
+            surrounding.append((r + dx, c + dy))
+    return surrounding
+
+
+def printMatrix(G, message=None):
+    if message != None:
+        print(message)
+    for r in G:
+        print(r)
+    print()
+
+
 if __name__ == '__main__':
-    print(readStringsBySeparator('strings_by_sep_test.txt','-'))
+    print(readStringsBySeparator('strings_by_sep_test.txt', '-'))
     print(readIntList('int_list.txt'))
