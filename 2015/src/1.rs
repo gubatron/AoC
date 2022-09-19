06ffb622481da25b949d8a21b14969bb1a87fac9
Angel Leon <gubatron@gmail.com>
2022-09-19 17:50:35 -0600

    2015/1 part 2

--- 2015/src/1.rs
+++ 2015/src/1.rs
@@ -13,6 +13,26 @@ fn part1() {
     println!("1. part 1: Current floor is {}", current_floor);
 }
 
+fn part2() {
+    let vec_char = utils::load_input_as_vec_char(String::from("src/1.txt"));
+    let mut current_floor = 0;
+    let mut current_character = 1;
+    for c in vec_char {
+        match c {
+            '(' =>current_floor += 1,
+            ')' =>current_floor -= 1,
+            _ => {}
+        }
+        if current_floor == -1 {
+            break;
+        }
+        current_character +=1;
+    }
+    println!("1. part 2: Current floor is {} and first character to get us there is {}", current_floor, current_character);
+}
+
+
 fn main() {
     part1();
+    part2();
 }
