3535cc344530768c01713827e1910e5ab880b9a6
Angel Leon <gubatron@gmail.com>
2022-09-20 23:59:46 -0600

    2015/2 part 2

--- 2015/src/2.rs
+++ 2015/src/2.rs
@@ -10,13 +10,27 @@ fn dimensions_to_rect_prism_surface_area_plus_smallest_side(tuple: (i32, i32, i3
     2 * (tuple.0 * tuple.1 + tuple.0 * tuple.2 + tuple.1 * tuple.2) + tuple.0 * tuple.1
 }
 
+fn dimensions_to_ribbons(t: (i32, i32, i32)) -> i32 {
+    t.0 * 2 + t.1 * 2 + (t.0 * t.1 * t.2)
+}
+
 fn part1() -> i32 {
-    let input_lines: Vec<String> = utils::load_input_lines_as_vec_str("src/2.txt");
-    input_lines.iter().
+    utils::load_input_lines_as_vec_str("src/2.txt").
+        iter().
         map(|line| map_to_int_tuple(line)).
-        map(|dimensions| dimensions_to_rect_prism_surface_area_plus_smallest_side(dimensions)).sum()
+        map(|dimensions| dimensions_to_rect_prism_surface_area_plus_smallest_side(dimensions)).
+        sum()
+}
+
+fn part2() -> i32 {
+    utils::load_input_lines_as_vec_str("src/2.txt").
+        iter().
+        map(|line| map_to_int_tuple(line)).
+        map(|dimensions| dimensions_to_ribbons(dimensions)).
+        sum()
 }
 
 fn main() {
     println!("{}", part1());
+    println!("{}", part2());
 }
