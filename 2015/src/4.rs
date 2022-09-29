e80930b80a5dd7eade03bbe656aa00155df6c104
Angel Leon <gubatron@gmail.com>
2022-09-29 14:54:24 -0600

    simpler

--- 2015/src/4.rs
+++ 2015/src/4.rs
@@ -1,10 +1,6 @@
 use md5::digest::core_api::CoreWrapper;
 use md5::{Digest, Md5, Md5Core};
 
-fn concat_secret_to_i32(secret: &str, n: i32) -> String {
-    format!("{}{}", secret, n)
-}
-
 fn md5hash_string(input: &String) -> String {
     let mut hasher: CoreWrapper<Md5Core> = Md5::new();
     hasher.update(input.as_bytes());
@@ -12,23 +8,26 @@ fn md5hash_string(input: &String) -> String {
 }
 
 fn find_adventcoin_nonce(secret: &String, has_six_zeroes: bool) -> i32 {
-    let mut i = 0;
+    let mut nonce = 0;
     loop {
-        let nonce = concat_secret_to_i32(secret, i);
-        let md5_hash_string = &md5hash_string(&nonce);
+        let hash_input = format!("{}{}", secret, nonce);
+        let md5_hash_string = &md5hash_string(&hash_input);
         if !has_six_zeroes && md5_hash_string.starts_with("00000") {
-            return i;
+            return nonce;
         } else if has_six_zeroes && md5_hash_string.starts_with("000000") {
-            return i;
+            return nonce;
         }
-        i += 1;
+        nonce += 1;
     }
 }
 
 #[test]
 fn tests() {
-    assert_eq!(find_adventcoin_nonce(&"abcdef".to_string()), 609043);
-    assert_eq!(find_adventcoin_nonce(&"pqrstuv".to_string()), 1048970);
+    assert_eq!(find_adventcoin_nonce(&"abcdef".to_string(), false), 609043);
+    assert_eq!(
+        find_adventcoin_nonce(&"pqrstuv".to_string(), false),
+        1048970
+    );
 }
 
 fn part1() {
