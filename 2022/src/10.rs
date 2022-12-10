013989d16a9d02c49151242f1ee99dd6ad694bc7
Angel Leon <gubatron@gmail.com>
2022-12-10 02:31:26 -0700

    2022/10 part 2

--- 2022/src/10.rs
+++ 2022/src/10.rs
@@ -8,6 +8,8 @@ fn main() {
     // test: part 1: 13140
     // real: part 1: 12740
     println!("part 1: {}", signal_strength_sum);
+
+    // part 2 should print: RBPARAGF
 }
 
 fn parse_program(program: Vec<String>, addx_instructions: &mut Vec<i32>) {
@@ -31,21 +33,34 @@ fn parse_program(program: Vec<String>, addx_instructions: &mut Vec<i32>) {
 }
 
 fn execute_instructions(mut addx_instructions: Vec<i32>) -> i32 {
-    let checkpoints = vec![20,60,100,140,180,220];
+    let checkpoints = vec![20, 60, 100, 140, 180, 220];
     let mut x = 1;
-    let mut pc = 1; // maybe starts at 0, gotta test
+    let mut pc = 1;
     let mut signal_sum = 0;
+    let mut screen = vec!['.'; 40 * 6];
 
     while addx_instructions.is_empty() == false {
         let addme = addx_instructions.remove(0);
         if checkpoints.contains(&pc) {
             signal_sum = signal_sum + (pc * x);
         }
-        println!("pc@{}: x={} addme={}",pc, x, addme);
         x += addme;
-        println!("pc@{}: x={}",pc, x);
-        println!();
+        for pixel in [x - 1, x, x + 1] {
+            if pc % 40 == pixel {
+                screen[pc as usize] = '#';
+            }
+        }
         pc += 1;
     }
+    print_screen(screen);
     signal_sum
+}
+
+fn print_screen(screen: Vec<char>) {
+    for y in 0..6 {
+        for x in 0..40 {
+            print!("{}", screen[y * 40 + x]);
+        }
+        println!();
+    }
 }
