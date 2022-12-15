0925ee72db69dfca86ab5f8e42fd6f4fb2e29f15
Angel Leon <gubatron@gmail.com>
2022-12-15 12:58:52 -0700

    2022/13 part 2. had to remove printing from the compare function to sort the packets

--- 2022/src/13.py
+++ 2022/src/13.py
@@ -8,21 +8,18 @@ def split_file_by_empty_lines(file_name):
         return lines.split('\n\n')
 
 
-def compare(left, right, indent):
-    indentation = ' ' * indent
-    print(indentation + '- Compare ' + str(left) + ' vs ' + str(right))
-
+def compare(left, right):
     if type(left) is int and type(right) is int:
         return left - right
 
     if type(left) is int and type(right) is list:
-        return compare([left], right, indent + 2)
+        return compare([left], right)
 
     if type(left) is list and type(right) is int:
-        return compare(left, [right], indent + 2)
+        return compare(left, [right])
 
     for left_item, right_item in zip(left, right):
-        cmp = compare(left_item, right_item, indent + 2)
+        cmp = compare(left_item, right_item)
         if cmp != 0:
             return cmp
     return len(left) - len(right)
@@ -35,14 +32,31 @@ if __name__ == '__main__':
             )
     )
 
-    results = 0
+    part_1 = 0
+    packets = [[[2]],[[6]]]
+
     for i, (left, right) in enumerate(signals):
+        packets.append(left)
+        packets.append(right)
+
         print(f"== Pair {i + 1} == ")
-        if compare(left, right, 0) < 0:
+        if compare(left, right) < 0:
             print(f"Pair {i + 1} is correct!")
-            results += i + 1
+            part_1 += i + 1
         print()
 
     # test:13
     # result: 5366
-    print(f"Part 1: {results}")
+    print(f"Part 1: {part_1}")
+
+    import functools
+    packets.sort(key=functools.cmp_to_key(compare))
+    part_2 = 1
+    for i, p in enumerate(packets):
+        if p == [[2]] or p == [[6]]:
+            part_2 *= (i+1)
+        print(str(i+1) + ' ' + str(p))
+
+    # test: 140
+    # result: 23391
+    print(f"Part 2: {part_2}")
