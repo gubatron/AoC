7acf4cdf71c63bb6f8918b210a2a9b62c85dbf07
Angel Leon <gubatron@gmail.com>
2023-08-29 20:11:25 -0600

    2015/7 simplification. still have endless loop on problem data. test from example passes

--- 2015/src/7.rs
+++ 2015/src/7.rs
@@ -1,245 +1,77 @@
 use aoc_2015::utils;
 use std::collections::HashMap;
 
-#[derive(Copy, Clone)]
-enum Instruction {
-    AND,
-    OR,
-    LSHIFT,
-    RSHIFT,
-    NOT,
-    NONE,
+fn get_value(circuit_map: &HashMap<String, u16>, s: &str) -> Option<u16> {
+    s.parse().ok().or_else(|| circuit_map.get(s).cloned())
 }
 
-#[derive(Clone)]
-struct gate {
-    input1: String,
-    input2: Option<String>,
-    instruction: Option<Instruction>,
-    signal: Option<u16>,
-}
+fn evaluate_instructions(mut instructions : Vec<String>) -> HashMap<String, u16> {
+  let mut circuit_map: HashMap<String, u16> = HashMap::new();
+  let mut i = 0;
+  while !instructions.is_empty() {
+    while i < instructions.len() {
+        let inst = &instructions[i];
+        if inst.contains("AND") || inst.contains("OR") {
+            let tokens : Vec<&str> = inst.split_whitespace().collect();
+            let (a, b, gate_id) = (tokens[0], tokens[2], tokens[4]);
 
-fn gate_has_signal(gate_id: &String, circuit_map: &HashMap<String, gate>) -> bool {
-    if let Some(g) = circuit_map.get(gate_id) {
-        if let Some(_) = g.signal {
-            return true;
-        }
-    }
-    false
-}
+            let a_in_circuit_map = circuit_map.contains_key(a);
+            let b_in_circuit_map = circuit_map.get(b) != None;
+            let a_is_numeric = a.parse::<u16>().is_ok();
+            let b_is_numeric = b.parse::<u16>().is_ok();
 
-fn gate_has_signal_on_both_inputs(gate_id: &String, circuit_map: &HashMap<String, gate>) -> bool {
-    if let Some(gate) = circuit_map.get(gate_id) {
-        // is the first input convertable to a u16
-        if let Ok(_) = gate.input1.parse::<u16>() {
-            // is the second input convertable to a u16
-            if let Ok(_) = gate.input2.as_ref().unwrap().parse::<u16>() {
-                return true;
-            } else {
-                return gate_has_signal(&gate.input2.as_ref().unwrap(), circuit_map);
+            if a_is_numeric || a_in_circuit_map {
+                if b_is_numeric || b_in_circuit_map {
+                    if inst.contains("AND") {
+                        circuit_map.insert(gate_id.to_string(), get_value(&circuit_map, a).unwrap() & get_value(&circuit_map, b).unwrap());
+                    } else if inst.contains("OR") {
+                        circuit_map.insert(gate_id.to_string(), get_value(&circuit_map, a).unwrap() | get_value(&circuit_map, b).unwrap());
+                    }
+                    instructions.remove(i);
+                    continue;
+                }
+            }        
+        } else if inst.contains("LSHIFT") || inst.contains("RSHIFT") {
+            let tokens : Vec<&str> = inst.split_whitespace().collect();
+            let (a, n, gate_id) = (tokens[0], tokens[2].parse::<u16>().unwrap(), tokens[4]);
+            if let Some(val) = get_value(&circuit_map, a) {
+                if inst.contains("LSHIFT") {
+                    circuit_map.insert(gate_id.to_string(), val << n);
+                } else if inst.contains("RSHIFT") {
+                    circuit_map.insert(gate_id.to_string(), val >> n);
+                }
+
+                instructions.remove(i);
+                continue;
+            }
+        } else if inst.contains("NOT") {
+            let tokens : Vec<&str> = inst.split_whitespace().collect();
+            let (a, gate_id) = (tokens[1], tokens[3]);
+            if let Some(val) = get_value(&circuit_map, a) {
+                circuit_map.insert(gate_id.to_string(), !val);
+                instructions.remove(i);
+                continue;
             }
         } else {
-            // first input is not a u16, so we need to check if it has a signal
-            if gate_has_signal(&gate.input1, circuit_map) {
-                // first input has a signal, so we need to check if the second input is a u16
-                if let Ok(_) = gate.input2.as_ref().unwrap().parse::<u16>() {
-                    return true;
-                } else {
-                    // second input is not a u16, so we need to check if it has a signal
-                    return gate_has_signal(&gate.input2.as_ref().unwrap(), circuit_map);
-                }
+            let tokens : Vec<&str> = inst.split(" -> ").collect();
+            let (a, gate_id) = (tokens[0], tokens[1]);
+            let a_is_numeric = a.parse::<u16>().is_ok();
+            let a_in_circuit_map = circuit_map.contains_key(a);
+            if a_is_numeric || a_in_circuit_map {
+                circuit_map.insert(gate_id.to_string(), get_value(&circuit_map, a).unwrap());
+                instructions.remove(i);
+                continue;
             }
         }
+        i += 1; 
     }
-    false
-}
-
-fn evaluate_gate(input1: u16, input2: u16, instruction: Instruction) -> Option<u16> {
-    match instruction {
-        Instruction::AND => Some(input1 & input2),
-        Instruction::OR => Some(input1 | input2),
-        Instruction::LSHIFT => Some(input1 << input2),
-        Instruction::RSHIFT => Some(input1 >> input2),
-        _ => None,
-    }
+  }
+  circuit_map
 }
 
 fn part1() -> i32 {
     let instructions = utils::load_input_lines_as_vec_str("src/7.txt");
-    let mut circuit_map: HashMap<String, gate> = HashMap::new();
-    let mut n = 0;
-    for i in instructions {
-        let v: Vec<&str> = i.split(" -> ").collect();
-        let id = v[1];
-        let instruction_string = v[0];
-        // if instruction string is an integer, then it's a value
-        if let Ok(value) = instruction_string.parse::<u16>() {
-            let g = gate {
-                input1: instruction_string.to_string(),
-                input2: None,
-                instruction: None,
-                signal: Some(value),
-            };
-            circuit_map.insert(id.to_string(), g);
-            continue;
-        } else {
-            // otherwise, it's a gate
-            let v2: Vec<&str> = instruction_string.split(" ").collect();
-
-            if v2[0] == "NOT" {
-                let g = gate {
-                    input1: v2[1].to_string(),
-                    input2: None,
-                    instruction: Some(Instruction::NOT),
-                    // if the circuit_map has a gate with the same id as input1, and it has a signal, our signal
-                    // will be the negation of that one
-                    signal: if let Some(g2) = circuit_map.get(&v2[1].to_string()) {
-                        if let Some(s) = g2.signal {
-                            Some(!s)
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    },
-                };
-                circuit_map.insert(id.to_string(), g);
-                continue;
-            }
-
-            // lx -> a
-            if v2.len() == 1 {
-                // check if the circuit already has a gate with id v2[0]
-                // if it does, then we need to copy that gate to the gate with id id
-                // if it doesn't, then we need to create a new gate with id id
-                if let Some(g) = circuit_map.get(&v2[0].to_string()) {
-                    circuit_map.insert(id.to_string(), g.clone());
-                } else {
-                    let g = gate {
-                        input1: v2[0].to_string(),
-                        input2: None,
-                        instruction: None,
-                        signal: None,
-                    };
-                    circuit_map.insert(id.to_string(), g);
-                }
-                continue;
-            }
-
-            //println!("i: {}. v2: {:?}", i, v2);
-
-            let input1 = v2[0];
-            let input2 = v2[2];
-            // operator/instruction in the middle
-            let instruction_opt: Option<Instruction> = match v2[1] {
-                "AND" => Some(Instruction::AND),
-                "OR" => Some(Instruction::OR),
-                "LSHIFT" => Some(Instruction::LSHIFT),
-                "RSHIFT" => Some(Instruction::RSHIFT),
-                _ => None,
-            };
-            //println!("instruction: {:?}", v2[1]);
-            //println!("instruction: {:?}", instruction);
-
-            // Easy case, both inputs are integers
-            if let Ok(val1) = input1.parse::<u16>() {
-                if let Ok(val2) = input2.parse::<u16>() {
-                    let g = gate {
-                        input1: input1.to_string(),
-                        input2: Some(input2.to_string()),
-                        instruction: instruction_opt,
-                        signal: evaluate_gate(val1, val2, instruction_opt.unwrap()),
-                    };
-                    circuit_map.insert(id.to_string(), g);
-                }
-            }
-            // input1 is u16 but input2 points to gate and it has signal
-            else if let Ok(val1) = input1.parse::<u16>() {
-                if gate_has_signal(&input2.to_string(), &circuit_map) {
-                    let gate2 = circuit_map.get(&input2.to_string()).unwrap();
-                    let val2 = gate2.signal.unwrap();
-                    let g = gate {
-                        input1: input1.to_string(),
-                        input2: Some(input2.to_string()),
-                        instruction: instruction_opt,
-                        signal: evaluate_gate(val1, val2, instruction_opt.unwrap()),
-                    };
-                    circuit_map.insert(id.to_string(), g);
-                }
-            }
-            // input2 is u16 but input1 points to gate and it has signal
-            else if let Ok(val2) = input2.parse::<u16>() {
-                if gate_has_signal(&input1.to_string(), &circuit_map) {
-                    let gate1 = circuit_map.get(&input1.to_string()).unwrap();
-                    let val1 = gate1.signal.unwrap();
-                    let g = gate {
-                        input1: input1.to_string(),
-                        input2: Some(input2.to_string()),
-                        instruction: instruction_opt,
-                        signal: evaluate_gate(val1, val2, instruction_opt.unwrap()),
-                    };
-                    circuit_map.insert(id.to_string(), g);
-                }
-            }
-            // input1 and input2 point to gates, and both have signals
-            else if gate_has_signal(&input1.to_string(), &circuit_map)
-                && gate_has_signal(&input2.to_string(), &circuit_map)
-            {
-                let gate1 = circuit_map.get(&input1.to_string()).unwrap();
-                let gate2 = circuit_map.get(&input2.to_string()).unwrap();
-                let val1 = gate1.signal.unwrap();
-                let val2 = gate2.signal.unwrap();
-                let g = gate {
-                    input1: input1.to_string(),
-                    input2: Some(input2.to_string()),
-                    instruction: instruction_opt,
-                    signal: evaluate_gate(val1, val2, instruction_opt.unwrap()),
-                };
-                circuit_map.insert(id.to_string(), g);
-            }
-            // input1 and input2 point to gates, but one or both don't have signals
-            else if !gate_has_signal(&input1.to_string(), &circuit_map)
-                || !gate_has_signal(&input2.to_string(), &circuit_map)
-            {
-                let g = gate {
-                    input1: input1.to_string(),
-                    input2: Some(input2.to_string()),
-                    instruction: instruction_opt,
-                    signal: None,
-                };
-                circuit_map.insert(id.to_string(), g);
-            }
-        }
-    } // for
-
-    // list of ids that need evaluating/updating
-    let ids_to_update: Vec<String> = circuit_map
-        .iter()
-        .filter(|(_, gate)| gate.signal.is_none())
-        .map(|(id, _)| id.clone())
-        .collect();
-
-    // evaluate
-    for id in ids_to_update {
-        let mut signal: Option<u16> = None;
-        if gate_has_signal_on_both_inputs(&id, &circuit_map) {
-            let gate = circuit_map.get(&id).unwrap();
-            let g1 = circuit_map.get(&gate.input1.to_string()).unwrap();
-            let g2 = circuit_map
-                .get(&gate.input2.as_ref().unwrap().to_string())
-                .unwrap();
-            signal = evaluate_gate(
-                g1.signal.unwrap(),
-                g2.signal.unwrap(),
-                gate.instruction.unwrap(),
-            );
-        }
-
-        if signal.is_some() {
-            let gate = circuit_map.get_mut(&id).unwrap();
-            gate.signal = signal;
-        }
-    }
+    let mut circuit_map = evaluate_instructions(instructions);
 
     // get all gate names in alphabetical order into a vector
     let mut gate_names: Vec<String> = circuit_map.iter().map(|(id, _)| id.clone()).collect();
@@ -248,16 +80,12 @@ fn part1() -> i32 {
 
     // print all gate names and their signals
     for name in gate_names {
-        println!("got a gate for '{}'?", name);
-        if let Some(gate) = circuit_map.get(&name) {
-            if let Some(signal) = gate.signal {
-                println!("{}: {:?}", name, gate.signal);
-            } else {
-                println!("{}: no signal yet", name);
-            }
+        if circuit_map.contains_key(&name) {
+            println!("{}: {:?}", name, get_value(&circuit_map, &name).unwrap());
+        } else {
+            println!("got a gate for '{}'?", name);
         }
     }
-
     0
 } // part1
 
