3ebe4af5980adcadbab171b47452f4b9f23e833b
Angel Leon <gubatron@gmail.com>
2023-09-12 09:25:59 -0600

    [2015/8] part 2

--- 2015/src/8.rs
+++ 2015/src/8.rs
@@ -22,6 +22,27 @@ fn part1() {
     }
     println!("Part 1: {}", total_code_chars - total_mem_chars);
 }
+
+fn part2() {
+    let inputs = utils::load_input_lines_as_vec_str("src/8.txt");
+    let mut total_code_chars = 0;
+    let mut total_encoded_chars = 0;
+    for input in inputs {
+        total_code_chars += input.len();
+        let mut chars = input.chars();
+        let mut encoded_chars = 0;
+        while let Some(c) = chars.next() {
+            if c == '\\' || c == '"' {
+                encoded_chars += 1;
+            }
+            encoded_chars += 1;
+        }
+        total_encoded_chars += encoded_chars + 2;
+    }
+    println!("Part 2: {}", total_encoded_chars - total_code_chars);
+}
+
 fn main() {
     part1(); // Part 1: 1371
+    part2(); // Part 2: 2117
 }
