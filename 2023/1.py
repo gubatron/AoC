d06074918156e2a31928bb88cdfabf7dbda55ada
Angel Leon <gubatron@gmail.com>
2023-12-02 20:30:25 -0700

    [2023] Day 1

---
+++ 2023/1.py
@@ -0,0 +1,58 @@
+from aoc import read_file_to_string_list
+
+
+def is_digit(c):
+    return ord(c) >= 48 and ord(c) <= 57
+
+
+def get_digits(input):
+    first, second = None, None
+    max_offset = len(input) - 1
+    i = 0
+    j = max_offset
+
+    while i < max_offset and j > 0:
+        left = input[i]
+        right = input[j]
+        if first is None and is_digit(left):
+            first = left
+        if second is None and is_digit(right):
+            second = right
+        i += 1
+        j -= 1
+
+    if second is None:
+        second = first
+    if first is None:
+        first = second
+    return first + second
+
+
+def part1():
+    input = read_file_to_string_list("1.txt")
+    print(sum(list(map(int, list(map(get_digits, input))))))
+
+
+def extract_digits(input):
+    digits = []
+    for i, c in enumerate(input):
+        if c.isdigit():
+            digits.append(c)
+        else:
+            for j, spelled in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+                if input[i:].startswith(spelled):
+                    digits.append(str(j + 1))
+    result = ''.join(digits)
+    if len(result) == 1:
+        result = result + result
+    result = result[0] + result[-1]
+    return int(result)
+
+
+def part2():
+    input = read_file_to_string_list("1.txt")
+    print(sum(map(extract_digits, input)))
+
+
+part1()
+part2()
