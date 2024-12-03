3197375188e282ef65466750dc8f5210b07d5926
Angel Leon <gubatron@gmail.com>
2024-12-03 14:40:39 -0700

    [day 1] part 2

--- 2024/src/one.rs
+++ 2024/src/one.rs
@@ -4,9 +4,23 @@ fn turn_tuples_into_2_vecs(input: Vec<(u32, u32)>) -> (Vec<u32>, Vec<u32>) {
     input.into_iter().unzip()
 }
 
-fn part1(a: Vec<u32>, b: Vec<u32>) -> u32 {
-    a.iter().zip(b).map(|(x, y)| x.abs_diff(y)).sum()
+fn part1(a: &Vec<u32>, b: &Vec<u32>) -> u32 {
+    a.iter().zip(b).map(|(x, y)| x.abs_diff(*y)).sum()
     // Part 1: 2057374
+}
+
+fn part2(a: &Vec<u32>, b: &Vec<u32>) -> u32 {
+    // Part 2: 23177084
+    let histogram_on_b = b
+        .iter()
+        .fold(std::collections::HashMap::new(), |mut acc, &x| {
+            *acc.entry(x).or_insert(0) += 1;
+            acc
+        });
+
+    a.iter()
+        .map(|x| x * histogram_on_b.get(x).unwrap_or(&0))
+        .sum()
 }
 
 fn main() {
@@ -26,5 +40,6 @@ fn main() {
     a_b_tuple.1.sort();
     let (a, b) = a_b_tuple;
 
-    println!("{}", format!("Part 1: {}", part1(a, b)));
+    println!("{}", format!("Part 1: {}", part1(&a, &b)));
+    println!("{}", format!("Part 2: {}", part2(&a, &b)));
 }
